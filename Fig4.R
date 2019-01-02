5f22f5fbe479722c672f9d7401f4f81e5551677a
vvzhukov <zhuko.v.italy@gmail.com>
2019-01-02 12:47:56 -0600

    models updated

--- Fig4.R
+++ Fig4.R
@@ -1,10 +1,11 @@
 ## Paper "Cross-disciplinary evolution of the genomics revolution"
 ## Fig 4
 ## Research reproduced by: Vitalii Z.
-## State: On progress
+## State: In progress
 
 ## Loading libraries, defining variables
 library(ggplot2)
+library(data.table)
 path <- "/Users/apple/UH-CPL/XD_Human_genom/Data_OSF" ## Path to data
 
 ## Uploading data 
@@ -12,13 +13,47 @@ path <- "/Users/apple/UH-CPL/XD_Human_genom/Data_OSF" ## Path to data
 data1 <- read.csv(paste(path,"/Faculty_GoogleScholar_Funding_Data_N4190.csv", sep=''),
                   header = TRUE)
 
+data2 <- read.csv(paste(path,"/ComputerScience_citations_stats_CitationNormalizationData.csv", sep=''),
+                  header = TRUE)
+
+data3 <- read.csv(paste(path,"/Biology_citations_stats_CitationNormalizationData.csv", sep=''),
+                  header = TRUE)
+
+## Google Scholar — publication and citation data
+data4 <- read.csv(paste(path,"/GoogleScholar_paper_stats.csv", sep=''),
+                  header = F)
+colnames(data4)<-c("google_id", "year", "citations", "coathor_codes")
+
+## CV+Network: Adding new column with citations summ data for each author, based on data4
+dt_data4 <- data.table(data4)
+dt_data4 <- dt_data4[,list(citations_summ=sum(citations)),by=google_id]
+merged_data <- merge(data1,dt_data4,by="google_id")
+
+## CV+Network (normalized): 
+Norm_data <- merge(data3, data4, all=T)
+dt_Norm_data <- dt_Norm_data[,list(citations_summ=sum(citations)),by=google_id]
+
 ## Linear model
-lm(log(t_pubs_citations)~log(SchoolRank)+
-                        log(h_index)+
-                        log(t_deflated_nsf),
-                        log(num_nsf)+
-                        log(t_deflated_nih)+
-                        log(num_nih)+
-                        log(PRCentrality)+
-                        log(Chi), 
+## Natural logarithm is only defined for x>0, log1p instead of log
+
+## Linear model for CV data
+mdl_cv <- lm(log1p(t_pubs_citations)~log1p(SchoolRank)+
+                        log1p(h_index)+
+                        log1p(t_deflated_nsf)+
+                        log1p(num_nsf)+
+                        log1p(t_deflated_nih)+
+                        log1p(num_nih)+
+                        log1p(PRCentrality)+
+                        log1p(Chi), 
                         na.action=na.exclude, data=data1)
+
+## Linear model for combined CV+Network data
+mdl_cv_nw <- lm(log1p(citations_summ)~log1p(SchoolRank)+
+                 log1p(h_index)+
+                 log1p(t_deflated_nsf)+
+                 log1p(num_nsf)+
+                 log1p(t_deflated_nih)+
+                 log1p(num_nih)+
+                 log1p(PRCentrality)+
+                 log1p(Chi), 
+             na.action=na.exclude, data=merged_data)
